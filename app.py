7544c9c50da971fa66a34811236e4fa90a425078
GeiGeiLa <kyokatsmin1909@hotmail.com>
2019-12-10 14:59:11 +0800

    can draw

--- app.py
+++ app.py
@@ -152,8 +152,9 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
+    print("drawing")
+    machine.get_graph().draw("fsm.svg", prog="dot", format="svg")
+    return send_file("fsm.svg", mimetype=".svg")
 
 
 
